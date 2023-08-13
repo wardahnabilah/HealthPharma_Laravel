5f34c47bb864119192349e6da61317a963a6ca30
unknown <wardahnabilah33@gmail.com>
2023-08-13 16:34:37 +0700

    Create medicines page

---
+++ resources/views/medicines.blade.php
@@ -0,0 +1,50 @@
+<x-layout>
+    <main class="container">
+        <h3>Daftar Obat</h3>
+        <div class="mb-3 text-end">
+            <a href="" class="btn btn-primary d-inline-flex gap-2 justify-content-center align-items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                  </svg>
+                Tambah Obat Baru
+            </a> 
+        </div>
+        <table class="table table-bordered border-dark text-center">
+            <thead>
+                <th>ID</th>
+                <th class="w-25">Nama Obat</th>
+                <th>Stok</th>
+                <th>Harga Satuan</th>
+                <th>Tanggal Expired</th>
+                <th></th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Vitamin C</td>
+                    <td>25</td>
+                    <td>5.000</td>
+                    <td>12 Agustus 2026</td>
+                    <td>
+                        {{-- Delete button --}}
+                        <a href="" class="btn btn-outline-danger btn-primary-sm">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                                <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
+                              </svg>
+                        </a>
+                        {{-- Edit button --}}
+                        <a href="" class="btn btn-outline-primary btn-primary-sm">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                            </svg>
+                        </a>
+                        {{-- Lihat Detail button --}}
+                        <a href="" class="btn btn-warning btn-primary-sm">Lihat Detail</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </main>
+</x-layout>
