9202ea13192f4906d5c9329d3879b7b84d86f5cf
unknown <wardahnabilah33@gmail.com>
2023-08-16 16:04:23 +0700

    Store image in storage folder and store its filename in database

--- app/Http/Controllers/MedicineController.php
+++ app/Http/Controllers/MedicineController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Medicine;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Storage;
 
 class MedicineController extends Controller
 {
@@ -22,15 +24,26 @@ class MedicineController extends Controller
             'namaObat' => ['required', 'min:3', Rule::unique('medicines', 'nama_obat')],
             'stokObat' => ['required', 'min:0', 'integer'],
             'hargaObat' => ['required', 'min:0', 'integer'],
-            'expiredObat' => ['required']
+            'expiredObat' => ['required'],
+            'gambarObat' => ['required', 'image', 'max:7000']
         ]);
+
+        // Resized the image
+        $resizedImage = Image::make($validatedReq['gambarObat'])->fit(125)->encode('jpg');
+
+        // Image's filename
+        $filename = $validatedReq['namaObat'] . '.jpg';
+
+        // Store the image in storage folder
+        Storage::put('public/avatar/' . $filename, $resizedImage);
 
         // Store in database
         $newMedicine = Medicine::create([
             'nama_obat' => $validatedReq['namaObat'],
             'stok' => $validatedReq['stokObat'],
             'harga' => $validatedReq['hargaObat'],
-            'tanggal_expired' => $validatedReq['expiredObat']
+            'tanggal_expired' => $validatedReq['expiredObat'],
+            'gambar_obat' => $filename
         ]);
         
         return redirect('/medicine/' . $newMedicine->id);
