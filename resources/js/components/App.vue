30dc48571bc2eca8b5e483a97a1028d10db65f46
unknown <wardahnabilah33@gmail.com>
2023-08-18 07:59:09 +0700

    Refactor order list table section on loggedin_homepage into vue component

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,8 +1,10 @@
 <script setup>
     import SearchMedicine from './SearchMedicine.vue';
+    import OrderListTable from './OrderListTable.vue';
 </script>
 <template>
-    <div>
+    <div class="row" >
         <SearchMedicine />
+        <OrderListTable />
     </div>
 </template>
--- resources/views/loggedin_homepage.blade.php
+++ resources/views/loggedin_homepage.blade.php
@@ -4,53 +4,8 @@
             <h3 class="mb-4">Buat Transaksi Baru</h3>
             <p class="login-as">Login sebagai: <span class="fw-bold">{{auth()->user()->username}}</span></p>
         </div>
-        <div class="row" id="app">
-            
-            <section class="col-5 ps-4">
-                <div class="border w-100" style="height: 32rem">
-                    <div class="d-flex justify-content-between px-3 py-2 border-bottom">
-                        <span>No. Pesanan 1</span>
-                        <span>12 Agustus 2023</span>
-                    </div>
-
-                    <table class="table table-bordered text-center fs-7">
-                        <thead class="table-dark">
-                            <tr>
-                                <th>No</th>
-                                <th>Nama Barang</th>
-                                <th>Harga Satuan</th>
-                                <th>Jumlah</th>
-                                <th>Harga</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>Vitamin C</td>
-                                <td>5.000</td>
-                                <td>2</td>
-                                <td>10.000</td>
-                                <td></td>
-                            </tr>
-                            <tr class="table-secondary">
-                                <td colspan="6"></td>
-                            </tr>
-                            <tr>
-                                <td colspan="2"></td>
-                                <td colspan="2">Total Harga</td>
-                                <td colspan="2">Rp 34.000,-</td>
-                            </tr>
-                            <tr>
-                                <td colspan="2"></td>
-                                <td colspan="2">Total Bayar</td>
-                                <td colspan="2"></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <button class="btn btn-secondary w-100 mt-5">TAMBAH</button>
-                </div>
-            </section>
+        <div id="app">
+            {{-- App.vue --}}
         </div>
     </main>
 </x-layout>
