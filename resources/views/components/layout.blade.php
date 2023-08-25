412d5d57253e75b7ff6a1ed34cc2c3ddf5b06cfc
unknown <wardahnabilah33@gmail.com>
2023-08-25 10:47:27 +0700

    Add document title and favicon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,9 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/favicon.ico">
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
-    <title>Document</title>
+    <title>Health Pharma</title>
 </head>
 <body class="d-flex flex-column min-vh-100">
     <header class="shadow-sm px-5 py-2">
